ad035a9d31d9d18f371167e5c43eb24358fdf8db
DenyskoD <deniskodima1@gmail.com>
2020-07-29 14:31:21 +0300

    header + intro
    full styles + js (nav toggle and fixed header)

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,1 +1,35 @@
-console.log(1);
+$(function() {
+
+    // fixed header    vars --> header , offset , scrollPosition
+
+    let header = $("#header");
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+    let offset = 150;
+    let scrollPosition = $(window).scrollTop();
+
+    checkScroll(scrollPosition, offset);
+
+    $(window).on("scroll", function(){
+        scrollPosition = $(this).scrollTop();
+        checkScroll(scrollPosition, offset);
+    });
+
+    function checkScroll(scrollPosition, offset) {
+        if(scrollPosition >= offset) { 
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }      
+
+    // nav toggle   vars --> nav , navToggle , header
+    
+
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+        header.toggleClass("mobile");
+        nav.toggleClass("show");
+    });
+
+});
